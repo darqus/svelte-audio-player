6f3a0927b44212d2c274a3e30d427970a52592a2
darqus <voladores@inbox.ru>
2024-12-20 23:09:24 +0300

    fix integration

--- vite.config.js
+++ vite.config.js
@@ -11,21 +11,10 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist', // Set output directory
-    assetsDir: 'assets', // Set assets directory
-    sourcemap: true, // Enable source maps
-    rollupOptions: {
-      output: {
-        format: 'iife', // Export as IIFE module for HTML integration
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-        name: 'AudioPlayer', // Global variable name for the library
-      },
-    },
     lib: {
       entry: 'src/main.js', // Entry point for the library
       name: 'AudioPlayer', // Global variable name for the library
-      formats: ['iife'],
+      formats: ['iife'], // Include both IIFE and ES formats
       fileName: 'svelte-audio-player',
     },
   },
