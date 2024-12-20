a3a909d7cbbd30f86a5a7e635ab11a7507677513
darqus <voladores@inbox.ru>
2024-12-20 16:10:03 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Set base to relative path
   plugins: [svelte()],
+  build: {
+    outDir: 'dist', // Set output directory
+    assetsDir: 'assets', // Set assets directory
+  },
 })
