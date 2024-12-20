a6611aa7382fd0bbf9563b4e0c265f619f8b5a59
darqus <voladores@inbox.ru>
2024-12-20 20:45:27 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
+// filepath: /home/eo/projects/github/audio-player/vite.config.js
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './', // Set base to relative path
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   build: {
     outDir: 'dist', // Set output directory
     assetsDir: 'assets', // Set assets directory
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
+    rollupOptions: {},
+    lib: {
+      entry: 'src/components/AudioPlayer.svelte',
+      name: 'AudioPlayer',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es'],
     },
   },
 })
